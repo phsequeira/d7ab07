968441f942bea00827058d9bb2640208fb6b28b1
Parker Sequeira <phsequeira@yahoo.com>
2022-04-11 19:24:07 +0000

    add read to db

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -67,13 +67,9 @@ router.get("/", async (req, res, next) => {
         convoJSON.otherUser.online = false;
       }
 
-      
-
       // set properties for notification count and latest message preview
       convoJSON.latestMessageText = convoJSON.messages[0].text;
       conversations[i] = convoJSON;
-      
-      convoJSON.messages.reverse();
     }
 
     res.json(conversations);
